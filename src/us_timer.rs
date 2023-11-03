5f1adab26548745f84abbb25f0b9c5a03f01c79f
Yogi Astawan <yogi.astawan@gmail.com>
2023-11-03 21:59:56 +0800

    add doc and fix typo

--- src/us_timer.rs
+++ src/us_timer.rs
@@ -1,8 +1,11 @@
+//!#Introduction
+//! This module containt traits that used to count number of ticks.
 
-
-
+///Trait that used to count number of ticks
 pub trait TickerUs {
+    ///Get number of current ticks
     fn get_tick(&self) -> u32;
 
+    ///Get frequency of tick
     fn get_frequency(&self) -> u32;
 }
